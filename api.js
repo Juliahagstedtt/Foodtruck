14375bd91ff93fb9f9fb14ed31ca45e2029329ec
Julia Hagstedt <juliahagstedt@hotmail.com>
2024-12-12 20:37:48 +0100

    api nyckel, funktioner med menu och cart. Ändringar m.m.

--- api.js
+++ api.js
@@ -1,32 +1,21 @@
-export const API_KEY = "yum-2ngXkR6S02ijFrTP";
-export const BASE_URL = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
+const apiKey = "yum-BHRyCR5Lgznl28Tr"; 
+const apiUrl = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/menu"; 
+const menuDiv = document.getElementById('menu');
+
+fetch('https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys', {
+   method: 'POST',
+   headers: {
+       'Accept': 'application/json',
+       'x-zocom': 'yum-BHRyCR5Lgznl28Tr',
+       'Content-Type': 'application/json'
+   },
+   body: JSON.stringify({ "name": "zocom" })
+})
+.then(response => response.json())
+.then(data => console.log(data)) 
+.catch(error => console.error('Error:', error));
 
 
 
-const apiUrl = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys';
-
-async function fetchApikey() {
- const response = await fetch ('${apiUrl}/keys', {
-    method: 'POST',
- });
- if (!response.ok) throw new Error('Error: ${response.status}');
- const data = await response.json();
- localStorage.setItem('apiKey', data.key);
- console.log('API-nyckel hämtad:', data.key);   
-}
-fetchApikey();
 
 
-async function createTenant(name) {
- const response = await fetch('${apiUrl}/tenants', {
-    method: 'POST',
-    headers: {
-        "Content-Type": 'application/json',
-        "x-zocom": apiKey,
-    },
- });
- if (!response.ok) throw new Error('Error: ${response.status}');
- const data = await response.json();
- console.log('Tenant skapad:', data);
-}
-createTenant('Julia');
--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-import { API_KEY, BASE_URL } from "./api.js";
-
 async function fetchMenu() {
-    const url = '${BASE-URL}/menu';
+    const apiKey = 'yum-BHRyCR5Lgznl28Tr'; // Ersätt med din API-nyckel
+    const response = await fetch("https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/menu", {
+        method: "GET",
+        headers: {
+            "x-zocom": apiKey
+        }
+    });
+    const data = await response.json();
+    console.log(data); 
+    displayMenu(data); 
 }
+
+
+function displayMenu(data) {
+    const menuItems = data.menuItems; 
+    menuItems.forEach(item => {
+        const itemElement = document.createElement("div");
+        itemElement.innerHTML = `
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <p>Pris: ${item.price} SEK</p>
+            <button onclick="addToCart(${item.id})">Lägg till i varukorg</button>
+        `;
+        document.getElementById("menu-container").appendChild(itemElement);
+        console.log(data);  // Se vad API:et returnerar
+
+    });
+}
+
+let cart = [];
+
+function addToCart(itemId) {
+    const item = menuItems.find(item => item.id === itemId);
+    cart.push(item);
+    updateCart();
+}
+
+function updateCart() {
+    const cartContainer = document.getElementById('cart-container');
+    cartContainer.innerHTML = '';
+
+    cart.forEach(item => {
+        const cartItem = document.createElement('div');
+        cartItem.innerHTML = '
+        <h4>${item.name}</h4>
+        <p>Pris: ${item.price}SEK</p>
+        ';
+        cartContainer.appendChild(cartItem);
+    });
+}
+
