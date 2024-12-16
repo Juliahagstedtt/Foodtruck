75f8815a960af909cf0f7e19c9589419ab73ff64
Julia Hagstedt <juliahagstedt@hotmail.com>
2024-12-16 01:57:33 +0100

    Navigering, style, Dom m.m.

---
+++ DOM.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navButtons = document.querySelectorAll("[data-view]"); 
+    const views = document.querySelectorAll(".view");
+
+    function showView(viewId) {
+
+        views.forEach(view => {
+            view.classList.remove("active");  
+        });
+
+        const viewToShow = document.getElementById(viewId);
+        if (viewToShow) {
+            viewToShow.classList.add("active");  
+        }
+    }
+
+    navButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            const viewId = event.target.closest("button").getAttribute("data-view");
+            showView(viewId);  
+        });
+    });
+
+    showView("menu");  
+});
